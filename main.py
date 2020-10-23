4557495291404b3dd570b37be153824b1f2a7211
Angela Yu <8798027+angelabauer@users.noreply.github.com>
2020-10-23 16:21:13 +0100

    Update and rename __init__.py to main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
     comment_author = relationship("User", back_populates="comments")
     text = db.Column(db.Text, nullable=False)
-db.create_all()
+# db.create_all()
 
 
 def admin_only(f):
